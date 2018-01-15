e5a6a55fcd0fc7f53f981c112d9bd7986826226a
УринбасаровД <@A-305-10>
2018-01-15 17:51:13 +0600

    first

---
+++ UserLogUp/LogInPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserLogUp
+{
+    /// <summary>
+    /// Логика взаимодействия для LogInPage.xaml
+    /// </summary>
+    public partial class LogInPage : Page
+    {
+        private MainWindow mainWindow;
+        public LogInPage()
+        {
+            InitializeComponent();
+        }
+
+        public LogInPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            this.mainWindow = mainWindow;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show();
+        }
+
+        //private void 
+    }
+}
